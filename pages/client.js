bb461e65dbf8e9ba86d4056422f00e0b531eca3c
pompeypigeon <pompeypigeon@gmail.com>
2018-01-25 12:14:57 +0000

    Letter selection developed more
    Users can choose to select a vowel or consonant depending on their text
    input (V or C, need to add more validation). Only 9 can be selected, as
    intended.

--- pages/client.js
+++ pages/client.js
@@ -2,20 +2,42 @@ const 	consonants = "BBCCCDDDDDDFFGGGHHJKLLLLLMMMMNNNNNNNNPPPPQRRRRRRRRRSSSSSSSS
 		vowels = "AAAAAAAAAAAAAAAEEEEEEEEEEEEEEEEEEEEEIIIIIIIIIIIIIOOOOOOOOOOOOOUUUUU",
 		lettersChosen = [];
 
+var		gameStarted = false;
+
 //query selectors
 const 	clock = document.querySelector('#clock'),
 		letters = document.querySelector('#letters'),
-		selectVowel = document.querySelector('#vowel'),
-		selectConsonant = document.querySelector('#consonant');
+		startGame = document.querySelector('#startGame');
+
 
 //event listeners
-selectVowel.addEventListener('click', drawVowel);
-selectConsonant.addEventListener('click', drawConsonant);
+startGame.addEventListener('click', startCountdown);
 
-function drawConsonant(){
-	lettersChosen.push(consonants.charAt(Math.floor(Math.random() * consonants.length)));
-}
+var userChoice;
 
-function drawVowel(){
-	lettersChosen.push(vowels.charAt(Math.floor(Math.random() * vowels.length)));
+function startCountdown(){
+	if(!gameStarted == true){
+		var chosen = 1, nextLetter;
+		for(chosen = 1; chosen < 10; chosen++){
+			var choice = prompt("Please choose a V for vowel or C for consonant", "").toUpperCase();
+			switch (choice){
+				case "V":
+					nextLetter = vowels.charAt(Math.floor(Math.random() * vowels.length));
+					lettersChosen.push(nextLetter);
+					letters.childNodes[chosen].innerText = nextLetter;
+					break;
+				case "C":
+					nextLetter = consonants.charAt(Math.floor(Math.random() * consonants.length));
+					lettersChosen.push(nextLetter);
+					letters.childNodes[chosen].innerText = nextLetter;
+					break;
+			}
+			console.log(nextLetter, lettersChosen);
+			letters.childNodes[chosen].innerText = nextLetter;
+			nextLetter = "";
+		}
+		
+	} else {
+		alert("Game already started");
+	}
 }
