23f5168cf71faf5f930ebbc6eb97b94921b8251e
gharrison121 <gharrison121@gmail.com>
2018-01-31 13:51:03 +0000

    Server side letter generation
    Vowels or consonants provided by server side generation using cumulative frequencies in a JSON file.

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
-/*const	express = 	require('express'),
-			db = 				require('./db'),
-			app = 			express(),
-			server = 		require('http').Server(app),
-			io = 				require('socket.io').listen(server),
-			port = 			process.env.PORT || 8080;
-
-server.listen(8080, function(){
-	console.log("On port 8080");
-});
-console.log("server running");
-
-app.use(express.static(__dirname + '/pages'));
-
-app.get('/', function(req,res){
-	res.sendFile(__dirname, "/pages/index.html");
-});*/
-
 'use strict'
 
 var express = require('express'),
 		app = express(),
-    db = require('./db'),
-    port = process.env.PORT || 8000,
+		fs = require('fs'),
+    port = process.env.PORT || 8080,
+
     server = app.listen(port, function(){
       console.log("server started on " + port)
     });
+
+
+var obj;
+fs.readFile('frequency.json', 'utf8', function(err, data) {
+	if (err) throw err;
+	obj = JSON.parse(data)
+	console.log(obj)
+	generateLetter(type) //called here so that the data is read before generating a letter - this is a dummy call
+});
+
+var type = "Consonant"; //dummy variable for testing before integration
+
+function generateLetter(type) {
+	var letter;
+	var randNum = Math.round(Math.random() * 1e3) / 1e3
+	var vowelValues = Object.entries(obj.Vowels)
+	var consonantValues = Object.entries(obj.Consonants)
+
+	if (type == "Vowel") {
+		for (var i = 0; i < vowelValues.length; i++) {
+			if (randNum < vowelValues[i][1]) {
+				letter = vowelValues[i][0]
+				console.log("letter is: " + letter)
+				break;
+			}
+		}
+
+	}
+	else {
+		for (var i = 0; i < consonantValues.length; i++) {
+			if (randNum < consonantValues[i][1]) {
+				letter = consonantValues[i][0]
+				console.log("letter is: " + letter)
+				break;
+			}
+		}
+	}
+}
+
+// generateLetter(type)
