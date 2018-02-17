3462331c86a9998e471b46ecc34e7827044f9d03
gharrison121 <gharrison121@gmail.com>
2018-02-17 00:40:48 +0000

    input validation
    pretty janky, no server-side validation

--- pages/js/game.js
+++ pages/js/game.js
@@ -3,7 +3,9 @@ var		gameStarted = false,
 			lettersChosen = [],
 			socket = io(),
 			time,
-			totalScore = 0;
+			totalScore = 0,
+			enteredLetters = [], // letters which client has entered
+			letterPool = []; //clone array of letters chosen for validation
 
 //query selectors
 const 	splashScreen = document.querySelector('#splashScreen'),
@@ -35,14 +37,11 @@ function startCountdown(){
 
 //socket to get a vowel from server
 function getVowel(){
-	
     socket.emit('letterRequest', 'Vowel');
-    
 }
 
 //socket to get a consonant from server
 function getConsonant(){
-    
     socket.emit('letterRequest', 'Consonant');
 }
 
@@ -52,6 +51,7 @@ socket.on('letterResponse', function(data){
 
     if (lettersChosen.length != 9){
 		lettersChosen.push(data);
+		letterPool.push(data)
 		letters.childNodes[lettersChosen.length].innerText = data;
 		console.log(lettersChosen)
 	} else {
@@ -91,8 +91,51 @@ function waitForCountdown(){
 	}
 }
 
+//validate user input test function
+function validAnswer(e) {
+
+		var inputValue = document.getElementById('playerWord')
+
+		/*loop through enteredLetters
+		If a letter is in enteredLetters but not in inputValue
+		add letter to letterPool
+		This allows letters to be recycled
+		This section has to go first to avoid losing letters from backspace
+		*/
+		for (var letter in enteredLetters) {
+			console.log("for loop run: " + enteredLetters )
+			if (!(letter in inputValue.value.split(""))) {
+				console.log("if statement run, letter pool is: " + letterPool)
+				letterPool.push(enteredLetters[letter])
+				enteredLetters.splice(letter, 1)
+			}
+		}
+		/*
+		Allow characters found in the letterPool, clone of lettersChosen to be modified
+		Block other characters except backspace
+		*/
+		if (letterPool.indexOf(String.fromCharCode(event.keyCode)) >= 0 || e.which === 8) {
+			//allow backspace
+			if (!(e.which === 8)) {
+				console.log("letter valid")
+				//add letter to array
+				enteredLetters.push(String.fromCharCode(event.keyCode))
+				console.log('entered letters: ' + enteredLetters)
+				//remove letter entered from letter pool
+				letterPool.splice(letterPool.indexOf(String.fromCharCode(event.keyCode)), 1)
+				return true;
+			}
+		} else {
+			console.log("letter invalid, removing")
+			return false;
+		}
+}
+
+
 function playAgain(){
 	lettersChosen = [];
+	letterPool = [];
+	enteredLetters = [];
 	startCountdown();
 	wasItRight.innerHTML = "";
 	letters.childNodes[9].innerText = "";
